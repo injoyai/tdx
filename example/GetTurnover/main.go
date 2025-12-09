04a586e2df39e699a3cdf1f76b994f41ecc78c18
injoyai <1113655791@qq.com>
2025-12-09 15:47:52 +0800

    测试换手率

---
+++ example/GetTurnover/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"time"
+
+	"github.com/injoyai/logs"
+	"github.com/injoyai/tdx"
+)
+
+func main() {
+
+	code := "sh688326"
+
+	c, err := tdx.DialDefault()
+	logs.PanicErr(err)
+
+	e, err := tdx.NewEquity(tdx.WithEquityClient(c))
+	logs.PanicErr(err)
+
+	resp, err := c.GetKlineDay(code, 0, 1)
+	logs.PanicErr(err)
+
+	t := e.Turnover(code, time.Now(), resp.List[0].Volume*100)
+
+	logs.Debug("换手率:", t)
+}
