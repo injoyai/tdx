84abbfe37504d5aba5e5f41c1dbc1d529019982e
injoyai <1113655791@qq.com>
2025-12-24 11:50:02 +0800

    优化合并

--- example/TradesToKlines/main.go
+++ example/TradesToKlines/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/injoyai/logs"
 	"github.com/injoyai/tdx"
 	"github.com/injoyai/tdx/example/common"
+	"github.com/injoyai/tdx/protocol"
 )
 
 func main() {
@@ -16,8 +17,29 @@ func main() {
 
 		ks := resp.List.Klines()
 
-		for _, v := range ks {
+		p := func(v *protocol.Kline) {
 			logs.Debug(v.Time.Format(time.TimeOnly), v.Last, v.Open, v.High, v.Low, v.Close, v.Volume, v.Amount)
+		}
+
+		for _, v := range ks {
+			p(v)
+		}
+
+		for _, v := range ks.Merge241(5) {
+			p(v)
+		}
+
+		for _, v := range ks.Merge241(15) {
+			p(v)
+		}
+
+		for _, v := range ks.Merge241(60) {
+			p(v)
+		}
+
+		ks = protocol.Klines{ks[0], ks[1], ks[2]}
+		for _, v := range ks.Merge241(60) {
+			p(v)
 		}
 
 	})
--- protocol/model_kline.go
+++ protocol/model_kline.go
@@ -3,10 +3,11 @@ package protocol
 import (
 	"errors"
 	"fmt"
-	"github.com/injoyai/base/types"
-	"github.com/injoyai/conv"
 	"sort"
 	"time"
+
+	"github.com/injoyai/base/types"
+	"github.com/injoyai/conv"
 )
 
 type KlineReq struct {
@@ -316,47 +317,133 @@ func (this Klines) Merge(n int) Klines {
 	return ks
 }
 
-//// Kline 计算多个K线,成一个K线
-//func (this Klines) Kline() *Kline {
-//	if this == nil {
-//		return new(Kline)
-//	}
-//	k := new(Kline)
-//	for i, v := range this {
-//		switch i {
-//		case 0:
-//			k.Open = v.Open
-//			k.High = v.High
-//			k.Low = v.Low
-//			k.Close = v.Close
-//		case len(this) - 1:
-//			k.Close = v.Close
-//			k.Time = v.Time
-//		}
-//		if v.High > k.High {
-//			k.High = v.High
-//		}
-//		if v.Low < k.Low {
-//			k.Low = v.Low
-//		}
-//		k.Volume += v.Volume
-//		k.Amount += v.Amount
-//	}
-//	return k
-//}
+// Merge241 合并成其他类型的K线
+func (ks Klines) Merge241(n int) Klines {
+	mDay := make(map[string]Klines)
+	for _, v := range ks {
+		day := v.Time.Format(time.DateOnly)
+		mDay[day] = append(mDay[day], v)
+	}
 
-//// Merge 合并K线,1分钟转成5,15,30分钟等
-//func (this Klines) Merge(n int) Klines {
-//	if this == nil {
-//		return nil
-//	}
-//	ks := []*Kline(nil)
-//	for i := 0; i < len(this); i += n {
-//		if i+n > len(this) {
-//			ks = append(ks, this[i:].Kline())
-//		} else {
-//			ks = append(ks, this[i:i+n].Kline())
-//		}
-//	}
-//	return ks
-//}
+	result := Klines{}
+
+	for _, dayKs := range mDay {
+		// 构建 minute → K 映射
+		m := make(map[string]*Kline, len(dayKs))
+		for _, k := range dayKs {
+			key := k.Time.Format(timeFormat)
+			m[key] = k
+		}
+
+		// 按 times 构建标准 241 根分钟
+		std := make([]*Kline, 0, len(times241))
+		var lastClose Price
+		for _, key := range times241 {
+			if k, ok := m[key]; ok {
+				std = append(std, k)
+				lastClose = k.Close
+			} else {
+				t, _ := time.ParseInLocation(timeFormat, key, time.Local)
+				std = append(std, &Kline{
+					Time:      t,
+					Open:      lastClose,
+					High:      lastClose,
+					Low:       lastClose,
+					Close:     lastClose,
+					Volume:    0,
+					Amount:    0,
+					Order:     0,
+					UpCount:   0,
+					DownCount: 0,
+				})
+			}
+		}
+
+		lenStd := len(std)
+		if lenStd == 0 {
+			continue
+		}
+
+		// 先把第一根独立 K 线直接加入结果
+		result = append(result, std[0])
+
+		// 从第二根开始 N 分钟合并
+		for i := 1; i < lenStd; {
+
+			k2 := &Kline{
+				Last:      std[i].Last,
+				Open:      std[i].Open,
+				High:      std[i].High,
+				Low:       std[i].Low,
+				Close:     std[i].Close,
+				Order:     std[i].Order,
+				Volume:    std[i].Volume,
+				Amount:    std[i].Amount,
+				Time:      std[i].Time,
+				UpCount:   std[i].UpCount,
+				DownCount: std[i].DownCount,
+			}
+
+			end := i + n
+			if end > lenStd {
+				end = lenStd
+			}
+
+			for j := i + 1; j < end; j++ {
+				k := std[j]
+				if k.High > k2.High {
+					k2.High = k.High
+				}
+				if k.Low < k2.Low {
+					k2.Low = k.Low
+				}
+				k2.Time = k.Time
+				k2.Close = k.Close
+				k2.Volume += k.Volume
+				k2.Amount += k.Amount
+				k2.Order += k.Order
+			}
+
+			result = append(result, k2)
+
+			i = end
+		}
+	}
+
+	return result
+}
+
+var (
+	times241 = []string{
+		// 上午 09:30 – 11:29
+		"09:30", "09:31", "09:32", "09:33", "09:34", "09:35", "09:36", "09:37", "09:38", "09:39",
+		"09:40", "09:41", "09:42", "09:43", "09:44", "09:45", "09:46", "09:47", "09:48", "09:49",
+		"09:50", "09:51", "09:52", "09:53", "09:54", "09:55", "09:56", "09:57", "09:58", "09:59",
+		"10:00", "10:01", "10:02", "10:03", "10:04", "10:05", "10:06", "10:07", "10:08", "10:09",
+		"10:10", "10:11", "10:12", "10:13", "10:14", "10:15", "10:16", "10:17", "10:18", "10:19",
+		"10:20", "10:21", "10:22", "10:23", "10:24", "10:25", "10:26", "10:27", "10:28", "10:29",
+		"10:30", "10:31", "10:32", "10:33", "10:34", "10:35", "10:36", "10:37", "10:38", "10:39",
+		"10:40", "10:41", "10:42", "10:43", "10:44", "10:45", "10:46", "10:47", "10:48", "10:49",
+		"10:50", "10:51", "10:52", "10:53", "10:54", "10:55", "10:56", "10:57", "10:58", "10:59",
+		"11:00", "11:01", "11:02", "11:03", "11:04", "11:05", "11:06", "11:07", "11:08", "11:09",
+		"11:10", "11:11", "11:12", "11:13", "11:14", "11:15", "11:16", "11:17", "11:18", "11:19",
+		"11:20", "11:21", "11:22", "11:23", "11:24", "11:25", "11:26", "11:27", "11:28", "11:29",
+		"11:30",
+
+		// 下午 13:01 – 15:00
+		"13:01", "13:02", "13:03", "13:04", "13:05", "13:06", "13:07", "13:08", "13:09", "13:10",
+		"13:11", "13:12", "13:13", "13:14", "13:15", "13:16", "13:17", "13:18", "13:19", "13:20",
+		"13:21", "13:22", "13:23", "13:24", "13:25", "13:26", "13:27", "13:28", "13:29", "13:30",
+		"13:31", "13:32", "13:33", "13:34", "13:35", "13:36", "13:37", "13:38", "13:39", "13:40",
+		"13:41", "13:42", "13:43", "13:44", "13:45", "13:46", "13:47", "13:48", "13:49", "13:50",
+		"13:51", "13:52", "13:53", "13:54", "13:55", "13:56", "13:57", "13:58", "13:59", "14:00",
+		"14:01", "14:02", "14:03", "14:04", "14:05", "14:06", "14:07", "14:08", "14:09", "14:10",
+		"14:11", "14:12", "14:13", "14:14", "14:15", "14:16", "14:17", "14:18", "14:19", "14:20",
+		"14:21", "14:22", "14:23", "14:24", "14:25", "14:26", "14:27", "14:28", "14:29", "14:30",
+		"14:31", "14:32", "14:33", "14:34", "14:35", "14:36", "14:37", "14:38", "14:39", "14:40",
+		"14:41", "14:42", "14:43", "14:44", "14:45", "14:46", "14:47", "14:48", "14:49", "14:50",
+		"14:51", "14:52", "14:53", "14:54", "14:55", "14:56", "14:57", "14:58", "14:59", "15:00",
+	}
+
+	timeFormat = "15:04"
+)
